a3cf81df8b7241c5941e9a10bb96715cb22f462e
Tyler Rocha <tyler.rocha@utexas.edu>
2017-02-07 16:44:44 +0800

    add toString for tagging class

--- src/seedu/addressbook/data/tag/Tagging.java
+++ src/seedu/addressbook/data/tag/Tagging.java
@@ -1,5 +1,32 @@
 package seedu.addressbook.data.tag;
 
+import seedu.addressbook.data.person.Person;
+
 public class Tagging {
 
+	private Tag tag;
+	private Person person;
+	
+	/** 
+	 * If true, this tag was added and a '+' will be printed.
+	 * If false, this tag was deleted and a '-' will be printed. 
+	 */
+	private boolean isAddedTag;
+	
+	public Tagging(Tag tag, Person person, boolean isAddedTag) {
+		this.tag = tag;
+		this.person = person;
+		this.isAddedTag = isAddedTag;
+	}
+	
+	public String getTagging() {
+		String sign = "";
+		if (isAddedTag) {
+			sign = "+ ";			
+		} else {
+			sign = "- ";
+		}
+		
+		return sign + person.getName().toString() + " " + tag.toString();
+	}
 }
