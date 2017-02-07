336dd7d33baf459b57e9e55436d0648df3e945a3
Tyler Rocha <tyler.rocha@utexas.edu>
2017-02-07 15:33:03 +0800

    add sequence number logic

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -15,6 +15,9 @@ public class Person implements ReadOnlyPerson {
     private Email email;
     private Address address;
 
+    private int sequenceNumber;
+    private static int nextSequenceNumber;
+    
     private final UniqueTagList tags;
     /**
      * Assumption: Every field must be present and not null.
@@ -25,9 +28,19 @@ public class Person implements ReadOnlyPerson {
         this.email = email;
         this.address = address;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+        this.sequenceNumber = getNextSequenceNumber();
     }
 
-    /**
+    /** 
+     * Assigns each person a unique number.
+     */
+    private int getNextSequenceNumber() {
+		int newSequenceNumber = nextSequenceNumber;
+		nextSequenceNumber++;
+		return newSequenceNumber;
+	}
+
+	/**
      * Copy constructor.
      */
     public Person(ReadOnlyPerson source) {
